a0a040818996da32ab6caaf4d86f384731f3e737
LiuWhale <abc541550872@live.com>
2019-10-12 13:50:22 +0800

    1.8.2-offboard_pub

--- cmake/configs/nuttx_px4fmu-v2_default.cmake
+++ cmake/configs/nuttx_px4fmu-v2_default.cmake
@@ -4,6 +4,7 @@ px4_nuttx_configure(HWCLASS m4 CONFIG nsh ROMFS y ROMFSROOT px4fmu_common IO px4
 
 set(config_module_list
 	modules/read_uart
+	modules/offboard_pub
 	#
 	# Board support modules
 	#
